c8f80c5ad90325f6b1ed4aeb6ea77f9e6406db6f
Rebekah Taylor <rebekahtdesigns@gmail.com>
2023-05-11 15:32:56 -0500

    madlib

--- site.php
+++ site.php
@@ -64,6 +64,29 @@
         <!-- http://localhost:4000/site.php?num1=4&num2=5 
         ^ Can manipulate variables with URL -->
 
+       
+       <!-- Mad Lib -->
+        <form action="site.php" method="get">
+            Color: <input type="text" name="color">
+            <br>
+            Plural noun: <input type="text" name="pluralNoun">
+            <br>
+            Celebrity: <input type="text" name="celebrity">
+            <br>
+            <input type="submit">
+        </form>
+
+        <?php
+        $color = $_GET["color"];
+        $pluralNoun = $_GET["pluralNoun"];
+        $celebrity = $_GET["celebrity"];
+
+        echo "Roses are $color <br>";
+        echo "$pluralNoun are blue <br>";
+        echo "I love $celebrity <br>";
+
+        ?>
+
     </body>
 </html>
 

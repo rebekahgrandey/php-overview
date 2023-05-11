2ebeece7baca712cb5c5d479696b96c2fd75c4c4
Rebekah Taylor <rebekahtdesigns@gmail.com>
2023-05-10 22:20:38 -0500

    echo and variables

--- site.php
+++ site.php
@@ -7,7 +7,14 @@
     <body>
 
         <?php
-            echo("Hello World!");
+            $characterName = "John";
+            $characterAge = 35;
+            echo "Hello, $characterName! You are $characterAge years old. ";
+            echo "$characterName, you're super cool for a $characterAge year old!!! ";
+            echo "<hr>";
+            $characterName = "Aly";
+            $characterAge = "28";
+            echo "$characterName is $characterAge years old."
         ?>
 
     </body>
